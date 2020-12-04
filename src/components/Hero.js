78231243c69d2946eb61e045049aef5ca98d39f4
canzrcolt <canzrcolt@gmail.com>
2020-12-03 23:33:02 -0500

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className="container">
+            <div className="hero-background">
+                <div className="hero-font"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
